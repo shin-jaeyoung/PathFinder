a99d0a916efe9278fb46bd0e873b3197360fb8f2
shin-jaeyoung <167179959+shin-jaeyoung@users.noreply.github.com>
2026-01-07 18:43:18 +0900

    feat skillInventory
    저장용

--- Assets/PathFinder/Scripts/Player/Inventory/PlayerSkillInventory.cs
+++ Assets/PathFinder/Scripts/Player/Inventory/PlayerSkillInventory.cs
@@ -6,17 +6,22 @@ public class PlayerSkillInventory : MonoBehaviour
 {
     [Header("Skill Inventory")]
     [SerializeField]
-    private List<string> skills;
+    private List<SkillSlot> skills;
     [SerializeField]
     private int capacity;
 
     private void Awake()
     {
-        skills = new List<string>();
+        skills = new List<SkillSlot>();
         for(int i = 0; i < capacity; i++)
         {
-            skills.Add("1");
+            skills.Add(new SkillSlot());
         }
     }
 
+    public bool AddSkill(Skill skill)
+    {
+
+        return true;
+    }
 }
