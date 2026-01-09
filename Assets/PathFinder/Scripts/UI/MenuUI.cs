c6c55be16f42f382e7207c18dd71b9497e3d969f
shin-jaeyoung <167179959+shin-jaeyoung@users.noreply.github.com>
2026-01-09 11:16:36 +0900

    feat UI
    UI매니저에서 팝업이나 패널들 스택으로관리
    메뉴 UI버튼 기능추가

---
+++ Assets/PathFinder/Scripts/UI/MenuUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public struct UIButtonData
+{
+    public UIType type;
+    public Button button;
+}
+public class MenuUI : MonoBehaviour
+{
+    [Header("Menu")]
+    [SerializeField]
+    private List<UIButtonData> buttons;
+
+    void Start()
+    {
+        if(UIManager.Instance != null)
+        {
+            foreach (var button in buttons)
+            {
+                button.button.onClick.RemoveAllListeners();
+                UIType type = button.type;
+                button.button.onClick.AddListener(()=>UIManager.Instance.ShowUI(type));
+                
+            }
+        }
+    }
+}
