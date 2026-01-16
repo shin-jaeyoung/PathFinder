ec9c996105c6b96d9f6cae5884bc0a9a374710f8
shin-jaeyoung <167179959+shin-jaeyoung@users.noreply.github.com>
2026-01-16 18:55:31 +0900

    Monster base

---
+++ Assets/PathFinder/Scripts/Monster/MonsterState.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MonsterState : State<Monster> { };
+
+public class MonsterIdleState : MonsterState 
+{
+    public override void Enter()
+    {
+        
+    }
+    public override void Update()
+    {
+        
+    }
+    public override void FixedUpdate()
+    {
+        
+    }
+    public override void Exit()
+    {
+
+    }
+}
+
+public class MonsterMoveState : MonsterState
+{
+    public override void Enter()
+    {
+
+    }
+    public override void Update()
+    {
+
+    }
+    public override void FixedUpdate()
+    {
+
+    }
+    public override void Exit()
+    {
+
+    }
+}
+public class MonsterHitState : MonsterState
+{
+    public override void Enter()
+    {
+
+    }
+    public override void Update()
+    {
+
+    }
+    public override void FixedUpdate()
+    {
+
+    }
+    public override void Exit()
+    {
+
+    }
+}
+
+public class MonsterAttackState : MonsterState
+{
+    public override void Enter()
+    {
+
+    }
+    public override void Update()
+    {
+
+    }
+    public override void FixedUpdate()
+    {
+
+    }
+    public override void Exit()
+    {
+
+    }
+}
+
+public class MonsterDeadState : MonsterState
+{
+    public override void Enter()
+    {
+
+    }
+    public override void Update()
+    {
+
+    }
+    public override void FixedUpdate()
+    {
+
+    }
+    public override void Exit()
+    {
+
+    }
+}
