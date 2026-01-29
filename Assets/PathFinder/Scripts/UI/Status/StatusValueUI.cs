24307e78f1fcada975a18d1745b0f00cc410ce8f
shin-jaeyoung <167179959+shin-jaeyoung@users.noreply.github.com>
2026-01-29 15:17:39 +0900

    feat StatusUI
    update statusSystem
    레벨포인트 소모로 스텟 올리는 STR,DEX,CON 기능추가
    STR,DEX,CON의 최종값을 가지고 기본능력치로 치환하는 기능 추가
    StatusUI로 시각화

---
+++ Assets/PathFinder/Scripts/UI/Status/StatusValueUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StatusValueUI : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI strValue;
+    [SerializeField]
+    private TextMeshProUGUI dexValue;
+    [SerializeField]
+    private TextMeshProUGUI conValue;
+    [SerializeField]
+    private TextMeshProUGUI powerValue;
+    [SerializeField]
+    private TextMeshProUGUI defValue;
+    [SerializeField]
+    private TextMeshProUGUI criRateValue;
+    [SerializeField]
+    private TextMeshProUGUI criDmgValue;
+
+
+    public void RefreshUI(PlayerStatusSystem stat)
+    {
+        strValue.text = stat.FinalStat[PlayerStatType.STR].ToString();
+        dexValue.text = stat.FinalStat[PlayerStatType.DEX].ToString();
+        conValue.text = stat.FinalStat[PlayerStatType.CON].ToString();
+        powerValue.text = stat.FinalStat[PlayerStatType.Power].ToString();
+        defValue.text = stat.FinalStat[PlayerStatType.Armor].ToString();
+        criRateValue.text = stat.FinalStat[PlayerStatType.CriRate].ToString();
+        criDmgValue.text = stat.FinalStat[PlayerStatType.CriDamage].ToString();
+    }
+
+
+}
