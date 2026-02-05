0e8219ea03502c502c7a61a80972a61a3b68213a
shin-jaeyoung <167179959+shin-jaeyoung@users.noreply.github.com>
2026-02-05 18:40:49 +0900

    UI sprite적용

--- Assets/PathFinder/Scripts/Npc/EndGame.cs
+++ Assets/PathFinder/Scripts/Npc/EndGame.cs
@@ -6,7 +6,10 @@ public class EndGame : MonoBehaviour, IInteractable
 {
     public void Interact(Player player)
     {
+        //여기에 저장기능 넣으면 될듯?
+
         //게임 종료
         Debug.Log("게임종료");
+        Application.Quit();
     }
 }
