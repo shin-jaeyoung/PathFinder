e98c591b40a832e5c2e64d696245ae63b524b230
shin-jaeyoung <167179959+shin-jaeyoung@users.noreply.github.com>
2026-02-11 16:25:16 +0900

    add GameStartScene, feat GameStartUI

---
+++ Assets/PathFinder/Scripts/UI/GameStartUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStartUI : MonoBehaviour
+{
+     
+    public void OnStart()
+    {
+        SceneManager.LoadScene(SceneType.Town.ToString());
+    }
+    public void OnEnd()
+    {
+        Debug.Log("게임을 종료");
+        Application.Quit();
+    }
+}
